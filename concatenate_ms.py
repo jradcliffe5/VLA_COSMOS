edd668640b398b04736dde9ae6b3ae5b440390ce
jradcliffe5 <jack.radcliffe@manchester.ac.uk>
2025-09-12 08:57:04 +0200

    new files

--- concatenate_ms.py
+++ concatenate_ms.py
@@ -12,7 +12,7 @@ delete=ast.literal_eval(sys.argv[i+1])
 if typ == '20B':
     ms = glob.glob('20B-370.sb*_sc.ms')
     
-    concat(vis=ms,concatvis='20B-370_ddf_sc.ms',timesort=True)
+    concat(vis=ms,concatvis='20B-370_target_sc.ms',timesort=True)
     
     if delete==True:
         for i in ms:
